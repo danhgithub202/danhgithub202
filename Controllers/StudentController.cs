814745870c5811b1e39c3a47461325de9fd046d0
danhgithub202 <daosongdanh2003tb@gmail.com>
2024-08-25 23:42:45 +0700

    add

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,13 @@
+
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+namespace DemoMVC.Controllers;
+public class StudentController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+    
+
+}
